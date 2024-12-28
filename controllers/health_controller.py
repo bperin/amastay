e20636beb5bf80f4fc49c34887ff62c7a672415f
Brian Perin <bperin42@gmail.com>
2024-12-28 02:28:18 -0800

    remove other stuff

--- controllers/health_controller.py
+++ controllers/health_controller.py
@@ -13,7 +13,7 @@ class HealthResponse(BaseModel):
     build_timestamp: str
 
 
-@router.get("/check", response_model=HealthResponse, operation_id="check")
+@router.get("/check", response_model=HealthResponse, operation_id="check", status_code=200)
 async def health_check():
-    """Health check endpoint that returns the status of the service and version"""
-    return {"status": "healthy", "message": "Service is running", "version": os.environ.get("GIT_HASH", "unknown"), "dockerfile_hash": os.environ.get("DOCKERFILE_HASH", "unknown"), "build_timestamp": os.environ.get("BUILD_TIMESTAMP", "unknown")}  # We'll add this to manifest
+    """Health check endpoint that returns the status of the service and a message"""
+    return {"status": "healthy", "message": "Service is running"}
